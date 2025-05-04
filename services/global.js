118c09ca53d3a3577cea8d09c415d904ccb1d75a
TrollingHoffman <Amithoffman56@gmail.com>
2025-05-04 11:54:13 +0300

    2nd Draft

--- services/global.js
+++ services/global.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const navLinks = document.querySelector('.nav-links');
     const hamburgerIcon = document.querySelector('.fa-bars-staggered');
